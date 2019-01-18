c01b3af8f35b2c3db3e0a85a6ee2b8569ac1fe4c
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-01-19 00:03:13 +0100

    Add WebView

--- App.js
+++ App.js
@@ -1,49 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react";
+import { Platform, StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <WebView
+        source={{ uri: "https://xdai.io" }}
+        style={Platform.OS === "ios" ? styles.iosOnly : styles.androidOnly}
+        onLoadProgress={e => console.log(e.nativeEvent.progress)}
+        useWebKit={true}
+        ref={r => (this.webview = r)}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        mixedContentMode={"compatibility"}
+        allowUniversalAccessFromFileURLs={true}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  iosOnly: {
+    marginTop: 20
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  androidOnly: {
+    marginTop: 0
+  }
 });
